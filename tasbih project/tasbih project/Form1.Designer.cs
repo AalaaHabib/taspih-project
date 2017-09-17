36ed131c270a399f1d79c65a4cb3d1f46f9fcdc6
AalaaHabib <30737217+AalaaHabib@users.noreply.github.com>
2017-09-17 23:18:08 +0200

    Add files via upload

---
+++ tasbih project/tasbih project/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace tasbih_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.اللهم_صلي = new System.Windows.Forms.Button();
+            this.العظم_وبحمدة = new System.Windows.Forms.Button();
+            this.استغفر_الله = new System.Windows.Forms.Button();
+            this.لا_حول = new System.Windows.Forms.Button();
+            this.الله_اكبر = new System.Windows.Forms.Button();
+            this.لا_اله_الا_الله = new System.Windows.Forms.Button();
+            this.الحمد_لله = new System.Windows.Forms.Button();
+            this.سبحان_الله = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // اللهم_صلي
+            // 
+            this.اللهم_صلي.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.اللهم_صلي.ForeColor = System.Drawing.Color.DarkRed;
+            this.اللهم_صلي.Location = new System.Drawing.Point(0, 237);
+            this.اللهم_صلي.Name = "اللهم_صلي";
+            this.اللهم_صلي.Size = new System.Drawing.Size(311, 30);
+            this.اللهم_صلي.TabIndex = 17;
+            this.اللهم_صلي.Text = "اللهم صلي ع محمد وع آل محمد";
+            this.اللهم_صلي.UseVisualStyleBackColor = true;
+            this.اللهم_صلي.Click += new System.EventHandler(this.اللهم_صلي_Click);
+            // 
+            // العظم_وبحمدة
+            // 
+            this.العظم_وبحمدة.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.العظم_وبحمدة.ForeColor = System.Drawing.Color.DarkRed;
+            this.العظم_وبحمدة.Location = new System.Drawing.Point(83, 163);
+            this.العظم_وبحمدة.Name = "العظم_وبحمدة";
+            this.العظم_وبحمدة.Size = new System.Drawing.Size(311, 30);
+            this.العظم_وبحمدة.TabIndex = 16;
+            this.العظم_وبحمدة.Text = "سبحان الله وبحمدة سبحان الله العظيم";
+            this.العظم_وبحمدة.UseVisualStyleBackColor = true;
+            this.العظم_وبحمدة.Click += new System.EventHandler(this.العظم_وبحمدة_Click);
+            // 
+            // استغفر_الله
+            // 
+            this.استغفر_الله.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.استغفر_الله.ForeColor = System.Drawing.Color.DarkRed;
+            this.استغفر_الله.Location = new System.Drawing.Point(0, 199);
+            this.استغفر_الله.Name = "استغفر_الله";
+            this.استغفر_الله.Size = new System.Drawing.Size(103, 32);
+            this.استغفر_الله.TabIndex = 15;
+            this.استغفر_الله.Text = "استغفر الله";
+            this.استغفر_الله.UseVisualStyleBackColor = true;
+            this.استغفر_الله.Click += new System.EventHandler(this.استغفر_الله_Click);
+            // 
+            // لا_حول
+            // 
+            this.لا_حول.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.لا_حول.ForeColor = System.Drawing.Color.DarkRed;
+            this.لا_حول.Location = new System.Drawing.Point(83, 72);
+            this.لا_حول.Name = "لا_حول";
+            this.لا_حول.Size = new System.Drawing.Size(215, 32);
+            this.لا_حول.TabIndex = 14;
+            this.لا_حول.Text = "لا حول ولا قوه الا بالله العلي العظيم";
+            this.لا_حول.UseVisualStyleBackColor = true;
+            this.لا_حول.Click += new System.EventHandler(this.لا_حول_Click);
+            // 
+            // الله_اكبر
+            // 
+            this.الله_اكبر.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.الله_اكبر.ForeColor = System.Drawing.Color.DarkRed;
+            this.الله_اكبر.Location = new System.Drawing.Point(328, 34);
+            this.الله_اكبر.Name = "الله_اكبر";
+            this.الله_اكبر.Size = new System.Drawing.Size(103, 32);
+            this.الله_اكبر.TabIndex = 13;
+            this.الله_اكبر.Text = "الله اكبر";
+            this.الله_اكبر.UseVisualStyleBackColor = true;
+            this.الله_اكبر.Click += new System.EventHandler(this.الله_اكبر_Click);
+            // 
+            // لا_اله_الا_الله
+            // 
+            this.لا_اله_الا_الله.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.لا_اله_الا_الله.ForeColor = System.Drawing.Color.DarkRed;
+            this.لا_اله_الا_الله.Location = new System.Drawing.Point(0, -4);
+            this.لا_اله_الا_الله.Name = "لا_اله_الا_الله";
+            this.لا_اله_الا_الله.Size = new System.Drawing.Size(103, 32);
+            this.لا_اله_الا_الله.TabIndex = 12;
+            this.لا_اله_الا_الله.Text = "لا اله الا الله ";
+            this.لا_اله_الا_الله.UseVisualStyleBackColor = true;
+            this.لا_اله_الا_الله.Click += new System.EventHandler(this.لا_اله_الا_الله_Click);
+            // 
+            // الحمد_لله
+            // 
+            this.الحمد_لله.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.الحمد_لله.ForeColor = System.Drawing.Color.DarkRed;
+            this.الحمد_لله.Location = new System.Drawing.Point(0, 34);
+            this.الحمد_لله.Name = "الحمد_لله";
+            this.الحمد_لله.Size = new System.Drawing.Size(103, 32);
+            this.الحمد_لله.TabIndex = 11;
+            this.الحمد_لله.Text = "الحمدلله ";
+            this.الحمد_لله.UseVisualStyleBackColor = true;
+            this.الحمد_لله.Click += new System.EventHandler(this.الحمد_لله_Click);
+            // 
+            // سبحان_الله
+            // 
+            this.سبحان_الله.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.سبحان_الله.ForeColor = System.Drawing.Color.DarkRed;
+            this.سبحان_الله.Location = new System.Drawing.Point(328, 2);
+            this.سبحان_الله.Name = "سبحان_الله";
+            this.سبحان_الله.Size = new System.Drawing.Size(103, 32);
+            this.سبحان_الله.TabIndex = 10;
+            this.سبحان_الله.Text = "سبحان الله";
+            this.سبحان_الله.UseVisualStyleBackColor = true;
+            this.سبحان_الله.Click += new System.EventHandler(this.سبحان_الله_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(269, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 32);
+            this.button1.TabIndex = 18;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkRed;
+            this.button2.Location = new System.Drawing.Point(269, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 32);
+            this.button2.TabIndex = 19;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.DarkRed;
+            this.button3.Location = new System.Drawing.Point(109, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(53, 32);
+            this.button3.TabIndex = 21;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.DarkRed;
+            this.button4.Location = new System.Drawing.Point(109, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(53, 32);
+            this.button4.TabIndex = 20;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.DarkRed;
+            this.button5.Location = new System.Drawing.Point(317, 235);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(53, 32);
+            this.button5.TabIndex = 23;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.DarkRed;
+            this.button6.Location = new System.Drawing.Point(109, 199);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(53, 32);
+            this.button6.TabIndex = 22;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.DarkRed;
+            this.button7.Location = new System.Drawing.Point(24, 163);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(53, 32);
+            this.button7.TabIndex = 24;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.DarkRed;
+            this.button8.Location = new System.Drawing.Point(24, 72);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(53, 32);
+            this.button8.TabIndex = 25;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::tasbih_project.Properties.Resources.images__4_;
+            this.ClientSize = new System.Drawing.Size(431, 264);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.اللهم_صلي);
+            this.Controls.Add(this.العظم_وبحمدة);
+            this.Controls.Add(this.استغفر_الله);
+            this.Controls.Add(this.لا_حول);
+            this.Controls.Add(this.الله_اكبر);
+            this.Controls.Add(this.لا_اله_الا_الله);
+            this.Controls.Add(this.الحمد_لله);
+            this.Controls.Add(this.سبحان_الله);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button اللهم_صلي;
+        private System.Windows.Forms.Button العظم_وبحمدة;
+        private System.Windows.Forms.Button استغفر_الله;
+        private System.Windows.Forms.Button لا_حول;
+        private System.Windows.Forms.Button الله_اكبر;
+        private System.Windows.Forms.Button لا_اله_الا_الله;
+        private System.Windows.Forms.Button الحمد_لله;
+        private System.Windows.Forms.Button سبحان_الله;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ tasbih project/tasbih project/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tasbih_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i;
+        private void سبحان_الله_Click(object sender, EventArgs e)
+        {
+            i++;
+            button1.Text = i.ToString();
+
+        }
+        int a;
+        private void الله_اكبر_Click(object sender, EventArgs e)
+        {
+            a++;
+            button2.Text = a.ToString();
+
+        }
+        int b;
+        private void لا_اله_الا_الله_Click(object sender, EventArgs e)
+        {
+            b++;
+            button4.Text = b.ToString();
+        }
+        int c;
+        private void الحمد_لله_Click(object sender, EventArgs e)
+        {
+            c++;
+            button3.Text = c.ToString();
+
+        }
+        int d;
+        private void لا_حول_Click(object sender, EventArgs e)
+        {
+            d++;
+            button8.Text = d.ToString();
+
+        }
+        int f;
+        private void العظم_وبحمدة_Click(object sender, EventArgs e)
+        {
+            f++;
+            button7.Text = f.ToString();
+
+        }
+        int y;
+        private void استغفر_الله_Click(object sender, EventArgs e)
+        {
+            y++;
+            button6.Text = y.ToString();
+
+        }
+        int k;
+        private void اللهم_صلي_Click(object sender, EventArgs e)
+        {
+
+            k++;
+            button5.Text = k.ToString();
+        }
+    }
+}
